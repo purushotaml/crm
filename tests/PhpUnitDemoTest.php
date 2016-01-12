d47a49cf0fab6009bf6b97c80c33f235924673ce
Purushotham Lingaraju <plingaraju@livetext.com>
2016-01-13 00:32:55 +0530

    n28

--- tests/PhpUnitDemoTest.php
+++ tests/PhpUnitDemoTest.php
@@ -4,11 +4,19 @@ require_once APPLICATION_HOME.'src/vendor/autoload.php';
 require_once APPLICATION_HOME.'src/PhpUnitDemo.php';
 class PhpUnitDemoTest extends PHPUnit_Framework_TestCase
 {	
+	/**
+	 * @covers PhpUnitDemo::add
+	 */
+	
 	function testUnSignedNumber() {
 		$w = new PhpUnitDemo();
 		$input = array("3","4");
 		$this->assertEquals(7, $w->add($input));
 	}
+	
+	/**
+	 * @covers PhpUnitDemo::add
+	 */
 	function testSignedNumber() {
 		$w = new PhpUnitDemo();
 		$input = array("-3","4");
