fabac4c889db35aa4dfcf8d8c6c9fa18b9539dae
Purushotham Lingaraju <plingaraju@livetext.com>
2016-01-14 20:37:43 +0530

    Added the annotation

--- src/PhpUnitDemo.php
+++ src/PhpUnitDemo.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * PhpUnitDemo Class
+ *
+ * This class is to demo the CI process for php projects using Jenkins
+ *
+ * @package      CI
+ * @subpackage   CI\PHPUNIT
+ * @category     Some Category
+ * @author       Purushotham Lingaraju
+ */
 class PhpUnitDemo {
 
 	public function add($input){	
